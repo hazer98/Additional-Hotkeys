7b9632140552a820d2115768edc69b3760d12b6c
Kasper Nilssen <kasper.nilssen1@gmail.com>
2021-12-30 00:00:10 +0100

    add tray icon with controls

--- ui/window.py
+++ ui/window.py
@@ -1,5 +1,6 @@
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtWidgets import QMainWindow, QPushButton, QWidget, QGridLayout, QVBoxLayout, QKeySequenceEdit, QHBoxLayout, \
-    QLineEdit, QSpacerItem, QSizePolicy
+    QLineEdit, QSpacerItem, QSizePolicy, QApplication, QMenu, QSystemTrayIcon, QStyle
 
 import cvars
 from ui.hotkey import Hotkey
@@ -11,9 +12,11 @@ class Window(QMainWindow):
         self.setWindowTitle('Windows Hotkeys')
         self.resize(cvars.WINDOW_WIDTH, cvars.WINDOW_HEIGHT)
         self.setMaximumHeight(600)
+        self.setWindowIcon(QIcon(cvars.ICON_PATH))
         self.setObjectName('window')
 
         self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
 
         self.main_layout = QVBoxLayout(self.central_widget)
 
@@ -21,23 +24,49 @@ class Window(QMainWindow):
 
         self.main_layout.addLayout(self.hotkeys_layout)
 
-        self.button = QPushButton('Add new')
+        self.button = QPushButton('Add new hotkey')
         self.button.setMaximumSize(100, 50)
         self.main_layout.addWidget(self.button)
 
         spacer = QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         self.main_layout.addItem(spacer)
 
-        self.setCentralWidget(self.central_widget)
+        self.setup_tray()
 
         self.load_stylesheet()
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
 
     def add_hotkey(self) -> Hotkey:
         hotkey = Hotkey()
         self.hotkeys_layout.insertWidget(0, hotkey)
         return hotkey
 
+    def setup_tray(self):
+        icon = QIcon(cvars.ICON_PATH)
+
+        tray = QSystemTrayIcon(self)
+        tray.setIcon(icon)
+        tray.setToolTip('Windows Hotkeys')
+        tray.setVisible(True)
+        tray.activated.connect(self.show)
+
+        show_action = QAction("Show", self)
+        quit_action = QAction("Exit", self)
+        hide_action = QAction("Hide", self)
+
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        quit_action.triggered.connect(QApplication.instance().quit)
+
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(quit_action)
+        tray.setContextMenu(tray_menu)
+
     def load_stylesheet(self):
         with open('ui/window.qss') as f:
             self.setStyleSheet(f.read())
-
