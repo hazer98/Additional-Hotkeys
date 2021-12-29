32ee88e79096c378fee83a5188d25ed435caa91d
Kasper Nilssen <kasper.nilssen1@gmail.com>
2021-12-29 19:24:14 +0100

    add cvars

--- ui/window.py
+++ ui/window.py
@@ -1,8 +1,11 @@
 from PyQt6.QtWidgets import QMainWindow
 
+import cvars
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-
-
+        self.setWindowTitle('Windows Hotkeys')
+        self.setFixedHeight(cvars.WINDOW_HEIGHT)
+        self.setFixedWidth(cvars.WINDOW_WIDTH)
