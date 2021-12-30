ad2f9bb63a4626f09b2998c98655cad553066ab5
Kasper Nilssen <kasper.nilssen1@gmail.com>
2021-12-30 01:08:26 +0100

    add helper methods for data reading

---
+++ utils.py
@@ -0,0 +1,14 @@
+import json
+
+import cvars
+
+
+def get_json_data() -> dict:
+    with open(cvars.DATA_PATH) as f:
+        data = json.load(f)
+    return data
+
+
+def save_json_data(data: dict):
+    with open(cvars.DATA_PATH, 'w') as f:
+        json.dump(data, f, indent=4)
