5d61964081795c35b89f13b9794cf07984540c85
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-01-08 17:52:53 +0100

    move section item to the section container wrapper

--- widgets/section_container.py
+++ widgets/section_container.py
@@ -3,7 +3,6 @@ from PyQt6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QFrame
 
 import cvars
 from utils.utils import resource_path
-from widgets.section_item import SectionItem
 
 
 class SectionContainer(QFrame):
@@ -103,3 +102,31 @@ class SectionContainer(QFrame):
             "widget": widget
         })
         self.update()
+
+
+class SectionItem(QFrame):
+    def __init__(self, widget):
+        super().__init__()
+
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setObjectName("section_item")
+
+        self.widget = widget
+
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.widget)
+
+        self.setLayout(self.layout)
+
+        self.load_stylesheet()
+
+    def load_stylesheet(self):
+        with open(resource_path('styles/section_item.qss')) as f:
+            self.setStyleSheet(f.read())
+
+    def delete(self):
+        print(self.widget)
+
+    def get_widget(self):
+        return self.widget
